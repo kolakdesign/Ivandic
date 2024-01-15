1ba66cfa13f5802fa604b519a3ffca0ec063eb7f
filipcro1 <filip.kolak@gmail.com>
2024-01-15 01:09:03 +0100

    Summary

---
+++ data.js
@@ -0,0 +1,319 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-d5_pano-hodnik-1-1_20240114_174015",
+      "name": "D5_Pano Hodnik 1 1_20240114_174015",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.10182991352292525,
+        "pitch": 0.14380555244206583,
+        "fov": 1.3662485491026943
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.77072781895213,
+          "pitch": 0.43894131597489405,
+          "rotation": 0,
+          "target": "1-d5_pano-radna_20240114_123914"
+        },
+        {
+          "yaw": 0.00008671879805177696,
+          "pitch": 0.06809608510688747,
+          "rotation": 0,
+          "target": "2-d5_pano-hodnik-2-2_20240114_173901"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-d5_pano-radna_20240114_123914",
+      "name": "D5_Pano Radna_20240114_123914",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.11660208170426323,
+        "pitch": 0.19244496636738617,
+        "fov": 1.3662485491026943
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2235328341730138,
+          "pitch": 0.2503570678163438,
+          "rotation": 0,
+          "target": "0-d5_pano-hodnik-1-1_20240114_174015"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-d5_pano-hodnik-2-2_20240114_173901",
+      "name": "D5_Pano Hodnik 2 2_20240114_173901",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.08825049416161868,
+        "pitch": 0.14172111050285174,
+        "fov": 1.3662485491026943
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.06506365503427958,
+          "pitch": 0.12910363070995423,
+          "rotation": 0,
+          "target": "3-d5_pano-dnevna_20240114_124138"
+        },
+        {
+          "yaw": 2.7712974833126642,
+          "pitch": -0.17822742254729462,
+          "rotation": 0,
+          "target": "5-d5_pano-stubite-2_20240114_173739"
+        },
+        {
+          "yaw": -3.089961133824122,
+          "pitch": 0.07119513136374067,
+          "rotation": 0,
+          "target": "0-d5_pano-hodnik-1-1_20240114_174015"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-d5_pano-dnevna_20240114_124138",
+      "name": "D5_Pano Dnevna_20240114_124138",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.6371502739444423,
+        "pitch": 0.06764641936126914,
+        "fov": 1.3662485491026943
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7464799084704392,
+          "pitch": 0.16188700288108393,
+          "rotation": 0,
+          "target": "4-d5_pano-kuhinja_20240114_124026"
+        },
+        {
+          "yaw": -1.5629600247030027,
+          "pitch": 0.17105273076411898,
+          "rotation": 0,
+          "target": "2-d5_pano-hodnik-2-2_20240114_173901"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-d5_pano-kuhinja_20240114_124026",
+      "name": "D5_Pano Kuhinja_20240114_124026",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.026755940417013235,
+        "pitch": 0.0707543079312547,
+        "fov": 1.3662485491026943
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.443334179954583,
+          "pitch": 0.2476266910399474,
+          "rotation": 0,
+          "target": "3-d5_pano-dnevna_20240114_124138"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-d5_pano-stubite-2_20240114_173739",
+      "name": "D5_Pano Stubište 2_20240114_173739",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.1647059480589057,
+        "pitch": 0.07047822342652843,
+        "fov": 1.3662485491026943
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1364302237985946,
+          "pitch": 0.7372597388821411,
+          "rotation": 0,
+          "target": "2-d5_pano-hodnik-2-2_20240114_173901"
+        },
+        {
+          "yaw": 1.8276227806565055,
+          "pitch": 0.18765982845016893,
+          "rotation": 0,
+          "target": "6-d5_pano-garderoba_20240114_123804"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-d5_pano-garderoba_20240114_123804",
+      "name": "D5_Pano Garderoba_20240114_123804",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08081741093982231,
+          "pitch": 0.0801459240544915,
+          "rotation": 0,
+          "target": "7-d5_pano-spavaa_20240114_123652"
+        },
+        {
+          "yaw": -3.031092388798065,
+          "pitch": 0.14828587643897784,
+          "rotation": 0,
+          "target": "5-d5_pano-stubite-2_20240114_173739"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-d5_pano-spavaa_20240114_123652",
+      "name": "D5_Pano Spavaća_20240114_123652",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.08641698319271995,
+        "pitch": 0.09849673670635184,
+        "fov": 1.3662485491026943
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.363411469901152,
+          "pitch": 0.1757916867877789,
+          "rotation": 0,
+          "target": "6-d5_pano-garderoba_20240114_123804"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
